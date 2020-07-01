826f41b2ec95924dee7830afe9995dba11ae0a88
Andrey Nedobylsky <admin@twister-vl.ru>
2020-07-01 13:30:41 +0300

    Update Utils.js

--- modules/helpers/Utils.js
+++ modules/helpers/Utils.js
@@ -37,6 +37,6 @@ module.exports = {
      * Generates random string
      */
     getid: () => {
-        (Math.random() * (new Date().getTime())).toString(36).replace(/[^a-z]+/g, '')
+        return (Math.random() * (new Date().getTime())).toString(36).replace(/[^a-z]+/g, '')
     }
 };
